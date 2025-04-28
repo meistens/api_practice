4548ab4b11d04dd4d632b6229c6d02b5c1191df2
David Mebo <mebodave@gmail.com>
2025-04-28 14:34:30 +0100

    Refactor healthcheck response from text to JSON format

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -8,7 +8,13 @@ import (
 // declare a handler which writes a plain-txt res with info
 // about the app status, operating env and version
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	// creating a fixed-format JSON res from a string literal
+	js := `{"status": "available", "environment": %q, "version": %q}`
+	js = fmt.Sprintf(js, app.config.env, version)
+
+	// setting 'content-type: application/json' header on the res
+	w.Header().Set("Content-Type", "application/json")
+
+	// write JSON as http res body
+	w.Write([]byte(js))
 }
