a4378ba2aa9951887f4802a0a1dc174b1ef8cdc3
David Mebo <mebodave@gmail.com>
2025-04-28 16:14:53 +0100

    Use MarshalIndent for formatted JSON output

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -29,7 +29,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 // containing any additional http requests to be inccluded in res
 func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
 	// encode data to json, returning error if there was one
-	js, err := json.Marshal(data)
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
