949627b04dc4702835d92af643884c51f8cd334b
David Mebo <mebodave@gmail.com>
2025-04-28 12:05:49 +0100

    Fix HandlerFunc method calls in router configuration

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -13,8 +13,8 @@ func (app *application) routes() *httprouter.Router {
 	// register relevant methods, patterns and handler funcs for
 	// the endpoints using handlerfunc()
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
-	router.HandleFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
-	router.HandleFunc(http.MethodGet, "v1/movies/:id", app.showMovieHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
+	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 
 	// return the httprouter instance
 	return router
