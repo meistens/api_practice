a57f6112d992fc73d6d4de23f6348014c6a43a0e
David Mebo <mebodave@gmail.com>
2025-04-28 12:17:50 +0100

    Remove duplicate code with new readIDParam helper

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // add createMovieHandler for the POST /v1/movies endpoint
@@ -13,17 +10,13 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	fmt.Fprintln(w, "create a new movie")
 }
 
-// add showMovieHandler for the GET /v1/movies/:id endpoint
+// showMovieHandler made simpler via readIDParams() in cmd/api/helpers.go
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
 
-	// convert to base10, if it cannot be converted or is less than 1
-	// return notFound()
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
-	// otherwise interpolate the movie id in a placeholder response
 	fmt.Fprintf(w, "show the details of movie %d\n", id)
 }
