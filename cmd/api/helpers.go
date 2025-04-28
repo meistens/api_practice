c1153c4e008f4066062701013ffd79045e3d20be
David Mebo <mebodave@gmail.com>
2025-04-28 15:18:10 +0100

    Add writeJSON helper for sending JSON responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
+
+	"maps"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -19,4 +22,26 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 		return 0, errors.New("invalid id param")
 	}
 	return id, nil
+}
+
+// defining a writejson() helper for sending responses
+// takes the dest responsewriter, status code to send, data to encode, and  header map
+// containing any additional http requests to be inccluded in res
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+	// encode data to json, returning error if there was one
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// append a newline for terminal applications
+	js = append(js, '\n')
+
+	maps.Copy(w.Header(), headers)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
