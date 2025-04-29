a98c336eeee78836aeb62c04ae61cb946d296e1f
David Mebo <mebodave@gmail.com>
2025-04-29 22:00:02 +0100

    Expand comment about Runtime type usage

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -9,7 +9,7 @@ type Movie struct {
 	Title     string    `json:"title"`          // Movie title
 	Year      int32     `json:"year,omitempty"` // Movie release year
 	// use Runtime type here, doing this is to wrap the int in a double quote
-	// while still making it an int (don't think too much about this...)
+	// while still making it an int (don't think too much about this... remove if it will be uncomfortable to use)
 	Runtime Runtime  `json:"runtime,omitempty"` // Movie runtime (in minutes)
 	Genres  []string `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
 	Version int32    `json:"version"`           // The version number starts at 1 and will be incremented each
