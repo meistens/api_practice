30b5351e523f46096379a9254d623fb78f3125cb
David Mebo <mebodave@gmail.com>
2025-05-03 20:55:57 +0100

    created a postgres connection pool (don't worry, this isn't a real password)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 // declare string containing semver
@@ -21,6 +25,11 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	// add a db struct field to hold the config settings for the db connection
+	// pool
+	db struct {
+		dsn string
+	}
 }
 
 // define app struct to hold deps for the HTTP handlers,
@@ -42,11 +51,29 @@ func main() {
 	// if no other flags are provided
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (dev|stage|prod)")
+	// read dsn value from the db-dsn flag into the config struct
+	// default flag dev is used if none is provided
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:greenlight12345@localhost/greenlight?sslmode=disable", "PostreSQL DSN")
+
 	flag.Parse()
 
 	// init. a new logger which writes to stdout
 	// prefixed with current date and time
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// call opendb() helper function to create conn. pool, passing the
+	// config struct
+	// if it returns an error, log it and exit
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	// defer a call to db.close() so that conn. pool is closed before
+	// main() exits
+	defer db.Close()
+
+	logger.Printf("conn. pool established")
 
 	// declare an instance of the app struct
 	// containing the config struct and logger
@@ -58,8 +85,8 @@ func main() {
 	// declare a new servemux and add a /v1/healthcheck route
 	// which dispatches requests to the healthcheckhanddler
 	// method
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 
 	// declare http server with some sensible timeout setting
 	// wich listens on the port provided in the config struct
@@ -76,6 +103,27 @@ func main() {
 	// start server
 	// sheesh, way too much stuff to do using a built-in pkg
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// opendb()
+func openDB(cfg config) (*sql.DB, error) {
+	// use sql.open() to create an empty conn. pool using dsn from the config struct
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// create context with a 5-sec timeout deadline
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// establish new conn. to db, pass context created as param
+	// if conn. isn't established within 5sec., return error
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/meistens/api_practice
 go 1.24.1
 
 require github.com/julienschmidt/httprouter v1.3.0
+
+require github.com/lib/pq v1.10.2 // indirect
