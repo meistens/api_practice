9e1aa6a183c9b8f2beec9625ab2919310688bb90
David Mebo <mebodave@gmail.com>
2025-05-04 23:15:32 +0100

    created placeholders for CRUD operations to be done

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"database/sql"
+	"time"
+)
 
 // NOTE -> CAPITALIZE FIRST LETTER MEANS TO BE VISIBLE WHEN EXPORTED!
 type Movie struct {
@@ -14,4 +17,29 @@ type Movie struct {
 	Genres  []string `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
 	Version int32    `json:"version"`           // The version number starts at 1 and will be incremented each
 	// time the movie information is updated
+}
+
+// struct wraps a conn. pool
+type MovieModel struct {
+	DB *sql.DB
+}
+
+// placeholder method for inserting new record(s)
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+// placeholder for fetchig specific record
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+// placeeholder for updating a specific record
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+// placeholder for deleting a specific record
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }
