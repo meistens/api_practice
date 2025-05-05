59ffda0d967096bfc8ce17f6772fc1f280d6af6d
David Mebo <mebodave@gmail.com>
2025-05-05 20:38:19 +0100

    added ValidateMovie func.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,6 +3,9 @@ package data
 import (
 	"database/sql"
 	"time"
+
+	"github.com/lib/pq"
+	"github.com/meistens/api_practice/internal/validator"
 )
 
 // NOTE -> CAPITALIZE FIRST LETTER MEANS TO BE VISIBLE WHEN EXPORTED!
@@ -24,9 +27,39 @@ type MovieModel struct {
 	DB *sql.DB
 }
 
-// placeholder method for inserting new record(s)
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "must not be in the future")
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be a positive integer")
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+// accepts a pointer to a movie struct, which creates data for the new record
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	// define sql query for inserting a new record in the movies table
+	// returns system-generated data
+	query := `INSERT INTO movies (title, year, runtime, genres)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id, created_at, version`
+
+	// create an arg slice containing the values for the placeholder params
+	// from the movie struct
+	// Declaring the slice immediately next to sql query helps
+	// make it nice and clear **what values are being used where**
+	// in the query
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	// queryrow() to execute the sql on conn. pool
+	// passing int the args slice as a varidic param and scanning
+	// the generated id, created_at and version into the movie struct
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 // placeholder for fetchig specific record
